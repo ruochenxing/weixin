94ac758505defd05a9010d0433b47cd695582d66
zdd <sherlock_65535@qq.com>
2017-05-01 14:30:53 +0800

    optimized code

--- src/main/java/com/weixin/servlet/MyServletWeixinSupport.java
+++ src/main/java/com/weixin/servlet/MyServletWeixinSupport.java
@@ -19,7 +19,10 @@ import com.weixin.util.SmtpHelper;
 
 //用户自行实现的微信消息收发处理器
 public class MyServletWeixinSupport extends WeixinSupport {
-	private static final String WELCOME_RESP_MESSAGE = "欢迎关注网盘搜索公众号！我是网盘搜索机器人，回复'help'查看功能。你也可以找我聊天哦，我什么都懂一点。";
+	
+	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(MyServletWeixinSupport.class);
+	
+	private static final String WELCOME_RESP_MESSAGE = "欢迎关注网盘搜索公众号！我是网盘搜索机器人，回复'help'查看功能。";
 	private static final String ERROR_RESP_MESSAGE = "哦，抱歉，我这边信号差，不知道你说什么，请重述一遍！";
 	private static final String CANNOT_REPLY_IMAGE = "哦抱歉，我看不懂图片";
 
@@ -45,7 +48,7 @@ public class MyServletWeixinSupport extends WeixinSupport {
 
 	@Override
 	protected BaseMsg handleTextMsg(TextReqMsg msg) {
-		System.out.println("text msg:" + msg);
+		logger.info("text msg:" + msg);
 		String content = msg.getContent();
 		BaseMsg result = beforeHandler(msg);
 		if (result != null) {
@@ -76,7 +79,7 @@ public class MyServletWeixinSupport extends WeixinSupport {
 		}
 		if (content.startsWith("搜索")) {
 			String key = content = content.replaceFirst("搜索(\\+|：|:)?", "");
-			System.out.println("搜索:" + key);
+			logger.info("搜索:" + key);
 			return new TextMsg(ArticleTool.replySearch(key));
 		}
 		return null;
@@ -84,31 +87,31 @@ public class MyServletWeixinSupport extends WeixinSupport {
 
 	@Override
 	protected BaseMsg handleImageMsg(ImageReqMsg msg) {
-		System.out.println("image msg:" + msg);
+		logger.info("image msg:" + msg);
 		return new TextMsg(CANNOT_REPLY_IMAGE);
 	}
 
 	@Override
 	protected BaseMsg handleVoiceMsg(VoiceReqMsg msg) {
-		System.out.println("voice msg:" + msg);
+		logger.info("voice msg:" + msg);
 		return super.handleVoiceMsg(msg);
 	}
 
 	@Override
 	protected BaseMsg handleLocationMsg(LocationReqMsg msg) {
-		System.out.println("location msg:" + msg);
+		logger.info("location msg:" + msg);
 		return super.handleLocationMsg(msg);
 	}
 
 	@Override
 	protected BaseMsg handleLinkMsg(LinkReqMsg msg) {
-		System.out.println("link msg:" + msg);
+		logger.info("link msg:" + msg);
 		return super.handleLinkMsg(msg);
 	}
 
 	@Override
 	protected BaseMsg handleSubscribe(BaseEvent event) {
-		System.out.println("subscribe event:");
+		logger.info("subscribe event:");
 		SmtpHelper.sendInExecutor(GlobalConstants.HOST_EMAIL, "公众号新增粉丝通知", "fromUserName:" + event.getFromUserName());
 		return new TextMsg(WELCOME_RESP_MESSAGE);
 	}
@@ -122,7 +125,7 @@ public class MyServletWeixinSupport extends WeixinSupport {
 
 	@Override
 	protected BaseMsg handleDefaultMsg(BaseReqMsg msg) {
-		System.out.println("response default msg:");
+		logger.info("response default msg:");
 		return new TextMsg(ERROR_RESP_MESSAGE);
 	}
 }
